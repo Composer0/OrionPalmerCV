f4336c83ffcbaedc4cb25372604b49a1fa6adeb5
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-03-06 10:01:54 -0500

    New additons
    Added smooth scroll with the aid of JQuery.
    Began adding projects section. 
    Images of project and direct links have been added. Next step will be to create a scroll up effect on card information. Perhaps even a way for searchers to find more of my work if they want to.

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 const open_btn = document.querySelector('.open-btn')
 const close_btn = document.querySelector('.close-btn')
 const nav = document.querySelectorAll('.nav')
+const info = document.querySelectorAll('.info')
 
 open_btn.addEventListener('click', () => {
     nav.forEach(nav_el => nav_el.classList.add('visible'))
-})
+});
 
 close_btn.addEventListener('click', () => {
     nav.forEach(nav_el => nav_el.classList.remove('visible'))
-})
+});
+
+info.addEventListener = ('click', () => {
+    document.getElementById("about").scrollIntoView({behavior: 'smooth'})
+});
+
+$("#info").click(function(){   //id of the link which is being clicked
+    $('html, body').animate({
+           scrollTop: $("#about").offset().top   //id of div to be scrolled
+    }, 1000);
+});
+
+$("#reach").click(function(){   //id of the link which is being clicked
+    $('html, body').animate({
+           scrollTop: $("#contact-me").offset().top   //id of div to be scrolled
+    }, 1000);
+});
+
+$("#the-work").click(function(){   //id of the link which is being clicked
+    $('html, body').animate({
+           scrollTop: $("#portfolio").offset().top   //id of div to be scrolled
+    }, 1000);
+});
